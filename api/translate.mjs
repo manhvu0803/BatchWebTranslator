586c984cce6217abdad833be49fdd49af5964a59
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2023-06-01 10:46:35 +0700

    Update translate API for CORS

--- api/translate.mjs
+++ api/translate.mjs
@@ -11,6 +11,23 @@ let endpoint = "https://api.cognitive.microsofttranslator.com";
 let location = "eastus2";
 let port = process.env.PORT || 3000;
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 export default async function handler(req, res) {
     let text = req.query.text;
 
