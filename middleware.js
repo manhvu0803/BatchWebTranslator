fe5c3a39ebae1cef312636475656f295f0ce6ca9
Vũ. Nguyễn Mạnh (4) <vunm4@vng.com.vn>
2023-07-04 16:37:43 +0700

    Add IP-blocking by middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,35 @@
+import { ipAddress, rewrite } from '@vercel/edge';
+
+let allowList = [
+    "::1",
+    "162\.158\.189\.",
+    "162\.158\.190\.",
+    "162\.158\.179\."
+]
+
 export default function middleware(req) {
     let url = new URL(req.url);
-    let ip = req.getHeader("x-forwarded-for");
-    let allowList = process.env.ALLOW_LIST;
+    let ip = ipAddress(req) || 'unknown';
     
-    if (!allowList || !allowList.includes(ip)) {
+    if (!allowList || !isAllow(ip)) {
         console.error(`Client ${ip} is not allowed in this service`);
-        url.pathname = "/blocked.html"
+        return new Response(`<h1>You do not have access to this page</h1> <h2>Your IP is ${ip}</h2>`, {
+            headers: { 'content-type': 'text/html' },
+        })
     }
     else {
-        url.pathname = "/index.html";
+        return rewrite(url, req.url);
+    }
+}
+
+function isAllow(ip) {
+    for (let allowedIp of allowList) {
+        let regex = new RegExp(allowedIp);
+
+        if (regex.test(ip)) {
+            return true;
+        }
     }
 
-    return Response.redirect(url)
+    return false;
 }
