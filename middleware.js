383ccc05dd3df00a869038bc89b6ccc0d4ff5e85
Vũ. Nguyễn Mạnh (4) <vunm4@vng.com.vn>
2023-08-11 09:54:50 +0700

    Add cookie to successful requests

--- middleware.js
+++ middleware.js
@@ -5,24 +5,34 @@ let allowList = [
     "162.158.189.",
     "162.158.190.",
     "162.158.179.",
+    "162.158.170.",
+    "162.158.106",
     "1.53.255.",
     "104.28.237.",
-    // Huy
-    "113.173.34."
+    // Huy P
+    "113.173.34.",
+    // Nha Trang
+    // This is temporary, should have cookie later
+    "113.160.248."
 ]
 
 export default function middleware(req) {
     let url = new URL(req.url);
     let ip = ipAddress(req) || 'unknown';
-    
-    if (!allowList || !isAllow(ip)) {
+    let cookie = req.cookies?.get("cookie") ?? "";
+    let secret = process.env?.SECRET ?? "a8698a6982bc1";
+
+    if (cookie !== secret && !isAllow(ip)) {
         console.error(`Client ${ip} is not allowed in this service`);
         return new Response(`<h1>You do not have access to this page</h1> <h2>Your IP is ${ip}</h2>`, {
             headers: { 'content-type': 'text/html' },
         })
     }
     else {
-        return rewrite(url, req.url);
+        let res = rewrite(url, req.url);
+        let expireDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 14);
+        res.headers.set("Set-Cookie", `cookie=${secret}; Expires=${expireDate.toUTCString()}`)
+        return res;
     }
 }
 
